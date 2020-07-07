ad382461c7cbff3082a6d87a74a38a1806d2a6f7
karuna-heks <zhenya.karuna@mail.ru>
2020-07-07 14:59:34 +0300

    07072020 added base for stopWords and tokenizer. need lemmatization and stemming

--- CorpusParser.py
+++ CorpusParser.py
@@ -1,27 +1,46 @@
 # -*- coding: utf-8 -*-
-
+import re
 
 
 class CorpusParser:
     
-    def __init__(self):
+    __tempWordList = None # список слов в тексте
+    __stopList = [] # список стоп-слов
+    __stopListEng = []
+    __stopListRus = []
+    
+    #@ params
+    __language = None
+    __stemType = None
+    __stopWordsType = None
+    
+    def __init__(self, language = 'eng', stemType = 'lemmatization',
+                 stopWordsType = 'default'):
         print("CP__init")
+        self.__language = language
+        self.__stemType = stemType
+        self.__stopWordsType = stopWordsType
+        if (self.__stopWordsType == 'default'):
+            #!!! продумать логику использования параметра стопВордс
+            self.__initStopWords()
+        
         
     def parsing(self, text):
         print("CPparsing")
         # получаем текст
         # отправляем в токенайзер
         # получаем список
-        # отправляем в удалитель небукв. символов
-        # делаем нижний регистр
         # удаляем стоп-слова из списка
         # отправляем список в стеммер/лемматизатор
         # возвращаем текст
+        
+        self.__tempWordList = self.__tokenizer(text)
+        
+        if (self.__stopWordsType == 'default'):
+            #!!! продумать логику использования параметра стопВордс
+            self.__tempWordList = self.__deleteStopWords(self.__tempWordList)
+        
         return text
-        
-        
-    def setParam(self):
-        print("CPsetParam")
         
         
          
@@ -35,13 +54,40 @@ class CorpusParser:
         
     def __tokenizer(self, text):
         print("CP__tokenizer")
+        text = text.lower()
+        text = re.sub(r"[^a-zа-яA-ZА-ЯёЁ0-9]+", " ", text)
+        # <- перевод в нижний регистр и замена небуквенных символов пробелами
+        # \b\W+\b|\b\W+$ - последовательности между словами (англ) и цифрами
+        # \b[\W0-9]+\b|\b\W+$ - последовательности между словами  (англ)
+        # [^a-zа-яA-ZА-ЯёЁ]+ - последовательности между словами русс и англ (любых)
+        # [^a-zа-яA-ZА-ЯёЁ0-9]+ - посл. межд. словами и цифрами любыми
+        # [^a-z]+ - последовательности между словами (англ)
         
+
+    
     def __deleteStopWords(self, wordList):
         print("CP__deleteStopWords")
+        #!!! реалиовать метод
         return wordList
         
-    def __deleteSymbols(self, wordList):
-        print("CP__deleteSymbols")
-        return wordList
+            
+    def __initStopWords(self):
+        print("CP__initStopWords")
+        if (self.__language == 'eng' | self.__language == 'mul'):
+            stopListEng = ['and', 'the', 'if', 'how', 'that', 
+                              'then', 'those', 'this', 'those',
+                              'can', 'be', 'will', 'would']
+            self.__stopList.extend(stopListEng)
+        if (self.__language == 'rus' | self.__language == 'mul'):
+            stopListRus = ['а', 'или', 'и', 'в', 'у', 'к',
+                                  'от', 'под', 'над', 'этот', 
+                                  'тот', 'те', 'их']
+            self.__stopList.extend(stopListRus)
+        #!!! дополнить список стоп-слов для обоих языков
+                              
+                          
+        
+        
+
     
     
--- tests.py
+++ tests.py
@@ -9,30 +9,62 @@ import sys
 
 print("test String 1")
 print("test String 3")
-sys.exit("fafs")
+# sys.exit("fafs")
 print("test String 2")
 
 text2 = "Пишем тут какой-нибудь текст. Посмотрим, как НЛТК с ним справится"
-f = open(r'/mnt/hgfs/vmware D/testRussian/output_file.txt', 'r')
-text = f.read()
-f.close()
+text3 = """The colour designations for these iron plates are as follows: \
+    1 kg is green, 1.5 kg is yellow, 2 kg is blue, 2.5 kg is red, \
+    5 kg and 0.5 kg are white. It is useful to note the colour assignment \
+    of these iron plates is consistent with the heavier bumper plates \
+    (i.e. 1 kg and 10 kg are green, 1.5 kg and 15 kg are yellow, etc.).\
+        Вот это поворот, конечно, в возникшей крайней ситуации... \
+            Что же теперь поделать, если 13 лет не могу справиться \
+                с последовательностями слов :4 вот это субститьюшн \
+                    авы у ук 33 43 а: авот? ав в Ашот в Ашон!Кек."""
+    
+    
+# f = open(r'/mnt/hgfs/vmware D/testRussian/output_file.txt', 'r')
+# text = f.read()
+# f.close()
 
+text = text3
+print("1\n\n")
+print(text)
 
 text = text.lower()
-text = re.sub(r"[^\w]", " ", text)
-text = re.sub(r"\s+", " ", text)
+print("2\n\n")
+print(text)
+
+text = re.sub(r"[^a-zа-яA-ZА-ЯёЁ]+", " ", text)
+# \b\W+\b|\b\W+$ - последовательности между словами (англ) и цифрами
+# \b[\W0-9]+\b|\b\W+$ - последовательности между словами  (англ)
+# [^a-zа-яA-ZА-ЯёЁ]+ - последовательности между словами русс и англ (любых)
+# [^a-zа-яA-ZА-ЯёЁ0-9]+ - посл. межд. словами и цифрами любыми
+# [^a-z]+ - последовательности между словами (англ)
+print("3\n\n")
+print(text)
+
+textList = text.split(" ")
+print("5\n\n")
+print(textList)
+
+textList.remove("for")
+print("6\n\n")
+print(textList)
+
 
 #sentences = nltk.sent_tokenize(text)
-words = nltk.word_tokenize(text, language = 'russian')
+# words = nltk.word_tokenize(text, language = 'russian')
 
-print(words)
-print()
+# print(words)
+# print()
 
-# os.system(r'./mystem')
+# # os.system(r'./mystem')
 
-# cmd = "mystem -l"
-# subprocess.Popen(cmd, shell = True)
+# # cmd = "mystem -l"
+# # subprocess.Popen(cmd, shell = True)
 
-mystem = Mystem()
-lemmas = mystem.lemmatize(text)
-print(''.join(lemmas))
+# mystem = Mystem()
+# lemmas = mystem.lemmatize(text)
+# print(''.join(lemmas))
